da35cb6fd991cbef8888e6b55bd03f809b528513
John Anthony Rico <johnrico364@gmail.com>
2026-02-09 00:58:06 +0800

    Backend: add image role and profile image column un user schema

--- server/modules/user/user.model.js
+++ server/modules/user/user.model.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    role_id: {
-      type: String,
-      enum: ["admin", "user", "super admin"],
-      required: true,
-    },
     f_name: { type: String, required: true },
     l_name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    profile_image: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "super admin", "operator", "terminal admin"],
+      default: "user",
+      required: true,
+    },
     status: { type: String, default: "active" },
   },
   { timestamps: true },
