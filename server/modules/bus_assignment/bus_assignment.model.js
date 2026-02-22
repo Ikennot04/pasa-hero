195e8cd77a836edd807eef09a532a5d92ffd3261
John Anthony Rico <johnrico364@gmail.com>
2026-02-22 17:14:25 +0800

    Update bus assignment model

--- server/modules/bus_assignment/bus_assignment.model.js
+++ server/modules/bus_assignment/bus_assignment.model.js
@@ -7,23 +7,22 @@ const busAssignmentSchema = new mongoose.Schema(
     operator_user_id: { type: String, ref: "Driver", required: true }, //Bus operator ni
     route_id: { type: String, ref: "Route", required: true },
 
-    assignment_date: { type: Date, required: true },
     status: {
       type: String,
       default: "scheduled",
       enum: [
         "scheduled",
         "active",
+        "inactive",
         "arrival_pending",
         "arrived",
-        "departed",
         "completed",
         "cancelled",
       ],
     },
 
-    arrival_time: { type: Date, default: null },
-    departure_time: { type: Date, default: null },
+    arrival_confirmed_by: { type: String, ref: "User", default: null }, // Terminal admin ID
+    arrival_confirmed_at: { type: Date, default: null },
   },
   { timestamps: true },
 );
