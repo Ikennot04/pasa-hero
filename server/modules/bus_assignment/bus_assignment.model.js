f6287045048362c37538e2887aecf8cad611af19
John Anthony Rico <johnrico364@gmail.com>
2026-02-27 14:06:11 +0800

    Change and refactor the bus assingment columns

--- server/modules/bus_assignment/bus_assignment.model.js
+++ server/modules/bus_assignment/bus_assignment.model.js
@@ -7,22 +7,32 @@ const busAssignmentSchema = new mongoose.Schema(
     operator_user_id: { type: String, ref: "Driver", required: true }, //Bus operator ni
     route_id: { type: String, ref: "Route", required: true },
 
-    status: {
+    assignment_status: {
       type: String,
-      default: "scheduled",
-      enum: [
-        "scheduled",
-        "active",
-        "inactive",
-        "arrival_pending",
-        "arrived",
-        "completed",
-        "cancelled",
-      ],
+      default: "active",
+      enum: ["active", "inactive"],
+    },
+    assignment_result: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "completed", "cancelled"],
     },
 
+    arrival_status: {
+      type: String,
+      default: "arrival_pending",
+      enum: ["arrival_pending", "arrived"],
+    },
     arrival_confirmed_by: { type: String, ref: "User", default: null }, // Terminal admin ID
     arrival_confirmed_at: { type: Date, default: null },
+    
+    departure_status: {
+      type: String,
+      default: "departure_pending",
+      enum: ["departure_pending", "departed"],
+    },
+    departure_confirmed_by: { type: String, ref: "User", default: null }, // Terminal admin ID
+    departure_confirmed_at: { type: Date, default: null },
   },
   { timestamps: true },
 );
