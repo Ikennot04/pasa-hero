5d54f93ec8936f0c386e3a477ae210bbde15dfda
John Anthony Rico <johnrico364@gmail.com>
2026-04-19 16:09:19 +0800

    API: Create a api that re order the bus stop

--- server/modules/route_stop/route_stop.route.js
+++ server/modules/route_stop/route_stop.route.js
@@ -4,11 +4,13 @@ import {
   getRouteStopsByRouteId,
   updateRouteStopById,
   deleteRouteStopById,
+  reorderRouteStops,
 } from "./route_stop.controller.js";
 
 const router = express.Router();
 
 router.get("/route/:routeId", getRouteStopsByRouteId);
+router.patch("/route/:routeId/reorder", reorderRouteStops);
 router.post("/", createRouteStop);
 router.patch("/:id", updateRouteStopById);
 router.delete("/:id", deleteRouteStopById);
